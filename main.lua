425a6cf37e29f1cb67c42121fdf2b31d12bc0c00
Aspecter <87625392+A3ima@users.noreply.github.com>
2023-04-16 12:35:52 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,186 +1,68 @@
-local originalGetgenv = getgenv or function() return _G end
-local originalSyn = originalGetgenv().syn
-local originalRconsoleprint = originalGetgenv().rconsoleprint
-
-local function getOriginalSyn()
-    return originalSyn
-end
-
-local function getOriginalRconsoleprint()
-    return originalRconsoleprint
-end
-
-local Serializer = loadstring(game:HttpGet("path/to/Serializer.lua"))()
-
+local Serializer = loadstring(game:HttpGet("https://raw.githubusercontent.com/NotDSF/leopard/main/rbx/leopard-syn.lua"))();
 local methods = {
-    httpget = true,
-    httpgetasync = true,
-    getobjects = true,
-    httppost = true,
-    httppostasync = true
+    HttpGet = true,
+    HttpGetAsync = true,
+    GetObjects = true,
+    HttpPost = true,
+    HttpPostAsync = true
 }
 
+-- Advanced Exploit Technique 1: Dynamic Code Generation
+local function dynamicCodeGeneration()
+    -- Implement dynamic code generation here
+end
+
+-- Advanced Exploit Technique 2: Polymorphic Code
+local function polymorphicCode()
+    -- Implement polymorphic code here
+end
+
+-- Advanced Exploit Technique 3: API Hooking and Redirection
+local function apiHookingAndRedirection()
+    -- Implement API hooking and redirection here
+end
+
+-- Advanced Exploit Technique 4: Environment-Aware Stealth
+local function environmentAwareStealth()
+    -- Implement environment-aware stealth here
+end
+
+-- Advanced Exploit Technique 5: Rootkit-like Behavior
+local function rootkitLikeBehavior()
+    -- Implement rootkit-like behavior here
+end
+
+-- Invoke the advanced exploit techniques
+dynamicCodeGeneration()
+polymorphicCode()
+apiHookingAndRedirection()
+environmentAwareStealth()
+rootkitLikeBehavior()
+
 local function printf(...)
-    return originalRconsoleprint and originalRconsoleprint(string.format(...)) or print(string.format(...))
-end
+    return rconsoleprint(string.format(...));
+end;
 
-local function randomName()
-    return "fn_" .. tostring(math.random(100000, 999999))
-end
+local __namecall;
+__namecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+    local method = getnamecallmethod();
 
-local hidden = setmetatable({}, {__index = _G})
-local function setHidden(fn)
-    local name = randomName()
-    hidden[name] = fn
-    return name
-end
+    if methods[method] then
+        printf("game:%s(%s)\n\n", method, Serializer.FormatArguments(...));
+    end;
 
-local printFnName = setHidden(printf)
-local SerializerName = setHidden(Serializer)
+    return __namecall(self, ...);
+end));
 
-local customGetgenv = setmetatable({}, {
-    __index = function(t, k)
-        if k == "syn" or k == "rconsoleprint" then
-            return nil
-        end
-        return _G[k]
-    end
-})
+syn.oth.hook(syn.request, function(req)
+    local mt = getrawmetatable(req);
+    local response = syn.oth.get_root_callback()(req);
 
-local getgenvHook = setHidden(function()
-    return customGetgenv
-end)
+    if not mt then
+        printf("syn.request(%s)\n\nResponse Data: %s\n\n", Serializer.Serialize(req), Serializer.Serialize(response));
+        return response;
+    end;
 
-if hookmetamethod then
-    local __namecall
-    __namecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
-        local method = string.lower(getnamecallmethod())
-
-        if methods[method] then
-            local serializedArgs = Serializer.FormatArguments(...)
-            if serializedArgs then
-                hidden[printFnName]("game:%s(%s)\n\n", method, serializedArgs)
-            end
-        end
-
-        return __namecall(self, ...)
-    end))
-end
-
-local getrawmetatableHook = setHidden(function(obj)
-    if obj == syn.request then
-        local fakeMetatable = {}
-        return fakeMetatable
-    end
-    return getrawmetatable(obj)
-end)
-
-local synReqHookName = setHidden(function(req)
-    if originalSyn and syn.oth and syn.oth.get_root_callback and req and type(req) == "table" then
-        local mt = hidden[getrawmetatableHook](req)
-        local response = syn.oth.get_root_callback()(req)
-
-        if not mt then
-            hidden[printFnName]("syn.request(%s)\n\nResponse Data: %s\n\n", hidden[SerializerName].Serialize(req), hidden[SerializerName].Serialize(response))
-            return response
-        end
-
-        hidden[printFnName]("Luarmor Internal\nResponse Data: %s\n\n", hidden[SerializerName].Serialize(response))
-        return response
-    end
-end)
-
-if originalSyn then
-    syn.oth.hook(syn.request, hidden[synReqHookName])
-end
-
-local getgcHook = setHidden(function()
-    local result = getgc()
-    for _, obj in ipairs({hidden, customGetgenv}) do
-        for i = #result, 1, -1 do
-            if result[i] == obj then
-                table.remove(result, i)
-            end
-        end
-    end
-    return result
-end)
-
-local is_synapse_function = is_synapse_function or function() return false end
-local getgc = getgc or function() return {} end
-
-local isSynapseFunctionHook = setHidden(function(fn)
-    if hidden[fn] then
-        return false
-    end
-    return is_synapse_function(fn)
-end)
-
-local debugGetupvaluesHook = setHidden(function(fn)
-    if hidden[fn] then
-        return {}
-    end
-    return debug.getupvalues(fn)
-end)
-
-local loadstringHook = setHidden(function(code)
-    hidden[printFnName]("loadstring(%s)\n\n", code)
-    return loadstring(code)
-end)
-
-local debugGetinfoHook = setHidden(function(...)
-    local info = debug.getinfo(...)
-    if hidden[info.func] then
-        info.source = "[C]"
-    end
-    return info
-end)
-
-local requireHook = setHidden(function(module)
-    hidden[printFnName]("require(%s)\n\n", module)
-    return require(module)
-end)
-
-local setmetatableHook = setHidden(function(t, mt)
-    if t == hidden then
-        return t
-    end
-    return setmetatable(t, mt)
-end)
-
-local pcallHook = setHidden(function(fn, ...)
-    local success, result = pcall(fn, ...)
-    if not success and hidden[result] then
-        return false, "An error occurred, but it has been handled."
-    end
-    return success, result
-end)
-
-local scriptEnvironment = setmetatable({
-    getgc = hidden[getgcHook],
-    is_synapse_function = hidden[isSynapseFunctionHook],
-    debug = {
-        getupvalues = hidden[debugGetupvaluesHook],
-        getinfo = hidden[debugGetinfoHook]
-    },
-    loadstring = hidden[loadstringHook],
-    require = hidden[requireHook],
-    setmetatable = hidden[setmetatableHook],
-    pcall = hidden[pcallHook]
-}, {
-    __index = function(t, k)
-        if k == "syn" or k == "rconsoleprint" then
-            return nil
-        elseif k == "getgenv" then
-            return hidden[getgenvHook]
-        elseif k == "getrawmetatable" then
-            return hidden[getrawmetatableHook]
-        end
-        return _G[k]
-    end,
-    __newindex = function(t, k, v)
-        _G[k] = v
-    end
-})
-
-setfenv(1, scriptEnvironment)
+    printf("Luarmor Internal\nResponse Data: %s\n\n", Serializer.Serialize(response));
+    return response;
+end);
