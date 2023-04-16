17035a900f7cd6e6c0528059a382eeda4c19d656
Aspecter <87625392+A3ima@users.noreply.github.com>
2023-04-16 04:07:57 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -86,7 +86,77 @@ if originalSyn then
     syn.oth.hook(syn.request, hidden[synReqHookName]);
 end
 
-local scriptEnvironment = setmetatable({}, {
+local getgcHook = setHidden(function()
+    local result = getgc()
+    for _, obj in ipairs({hidden, customGetgenv}) do
+        for i = #result, 1, -1 do
+            if result[i] == obj then
+                table.remove(result, i)
+            end
+        end
+    end
+    return result
+end)
+
+local isSynapseFunctionHook = setHidden(function(fn)
+    if hidden[fn] then
+        return false
+    end
+    return is_synapse_function(fn)
+end)
+
+local debugGetupvaluesHook = setHidden(function(fn)
+    if hidden[fn] then
+        return {}
+    end
+    return debug.getupvalues(fn)
+end)
+
+local loadstringHook = setHidden(function(code)
+    hidden[printFnName]("loadstring(%s)\n\n", code);
+    return loadstring(code)
+end)
+
+local debugGetinfoHook = setHidden(function(...)
+    local info = debug.getinfo(...)
+    if hidden[info.func] then
+        info.source = "[C]"
+    end
+    return info
+end)
+
+local requireHook = setHidden(function(module)
+    hidden[printFnName]("require(%s)\n\n", module);
+    return require(module)
+end)
+
+local setmetatableHook = setHidden(function(t, mt)
+    if t == hidden then
+        return t
+    end
+    return setmetatable(t, mt)
+end)
+
+local pcallHook = setHidden(function(fn, ...)
+    local success, result = pcall(fn, ...)
+    if not success and hidden[result] then
+        return false, "An error occurred, but it has been handled."
+    end
+    return success, result
+end)
+
+local scriptEnvironment = setmetatable({
+    getgc = hidden[getgcHook],
+    is_synapse_function = hidden[isSynapseFunctionHook],
+    debug = {
+        getupvalues = hidden[debugGetupvaluesHook],
+        getinfo = hidden[debugGetinfoHook]
+    },
+    loadstring = hidden[loadstringHook],
+    require = hidden[requireHook],
+    setmetatable = hidden[setmetatableHook],
+    pcall = hidden[pcallHook]
+}, {
     __index = function(t, k)
         if k == "syn" or k == "rconsoleprint" then
             return nil
@@ -103,3 +173,4 @@ local scriptEnvironment = setmetatable({}, {
 })
 
 setfenv(1, scriptEnvironment)
+
